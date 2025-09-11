e2560735125b289f2ec5a6b995c805a1729f133e
Peter Francois <peter.francois237@hotmail.com>
2025-09-11 14:26:24 +0200

    feat: use ParseIntPipe to change the param 'id' ifrom a string to a number

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -5,13 +5,15 @@ import {
   Get,
   NotFoundException,
   Param,
+  ParseIntPipe,
   Post,
   Put,
 } from '@nestjs/common';
 import { StudentService } from './student.service';
 import type { CreateStudentDto } from './Dtos/create.student.dto';
-import type { ResponseInterface } from './interface/response.interface';
+import type { ResponseInterface } from '../utils/interface/response.interface';
 import type { UpdateStudentDto } from './Dtos/update.student.dto';
+import { StudentInterface } from './interface/student.interface';
 
 @Controller('students')
 export class StudentController {
@@ -19,10 +21,13 @@ export class StudentController {
 
   // * `GET /students` - Récupérer tous les étudiants
   @Get()
-  getAll(): ResponseInterface {
+  getAll(): ResponseInterface<{ students: StudentInterface[] }> {
     const students = this.studentService.getStudents();
     if (students.length == 0) {
-      return { data: {}, message: `Il n'y a pas d'étudiant dans la liste` };
+      return {
+        data: { students },
+        message: `Il n'y a pas d'étudiant dans la liste`,
+      };
     }
     return {
       data: { students },
@@ -32,8 +37,11 @@ export class StudentController {
 
   // * `GET /students/:id` - Récupérer un étudiant par son ID
   @Get(':id')
-  getById(@Param('id') id: string): ResponseInterface {
-    const student = this.studentService.getStudentById(+id);
+  getById(
+    @Param('id', ParseIntPipe) id: number,
+  ): ResponseInterface<{ student: StudentInterface }> {
+    console.log(typeof id);
+    const student = this.studentService.getStudentById(id);
     if (!student) {
       throw new NotFoundException(
         `L'id ${id} n'existe pas dans la liste des étudiants`,
@@ -47,7 +55,10 @@ export class StudentController {
 
   // * `POST /students` - Créer un nouvel étudiant
   @Post()
-  create(@Body() body: CreateStudentDto): ResponseInterface {
+  create(@Body() body: CreateStudentDto): ResponseInterface<{
+    student: StudentInterface;
+    students: StudentInterface[];
+  }> {
     const student = this.studentService.createStudent(body);
     const students = this.studentService.getStudents();
     //throw NotfoundExeption
@@ -60,10 +71,10 @@ export class StudentController {
   // * `PUT /students/:id` - Mettre à jour un étudiant existant
   @Put(':id')
   update(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body() body: UpdateStudentDto,
-  ): ResponseInterface {
-    const student = this.studentService.updateStudent(+id, body);
+  ): ResponseInterface<{ student: StudentInterface }> {
+    const student = this.studentService.updateStudent(id, body);
     return {
       data: { student },
       message: `L'étudiant ${student.firstName} a était mis à jour`,
@@ -71,11 +82,10 @@ export class StudentController {
   }
   // * `DELETE /students/:id` - Supprimer un étudiant
   @Delete(':id')
-  delete(@Param('id') id: string): ResponseInterface {
-    const student = this.studentService.deleteStudent(+id);
-    const students = this.getAll();
+  delete(@Param('id', ParseIntPipe) id: number): ResponseInterface<null> {
+    const student = this.studentService.deleteStudent(id);
     return {
-      data: { students },
+      data: null,
       message: `L'étudiant ${student.firstName} a était retiré de la liste des étudiants`,
     };
   }
