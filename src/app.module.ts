79f5ed2fa81aee8237978861510c948d0ecd2e7b
Peter Francois <peter.francois237@hotmail.com>
2025-09-17 06:50:03 +0200

    feat: initial crud for TeamMember using nest generate res

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { StudentModule } from './student/student.module';
 import { FormationModule } from './formation/formation.module';
 import { PrismaModule } from 'prisma/prisma.module';
+import { TeamMemberModule } from './team-member/team-member.module';
+import { GroupeModule } from './groupe/groupe.module';
 
 @Module({
-  imports: [StudentModule, FormationModule, PrismaModule],
+  imports: [
+    StudentModule,
+    FormationModule,
+    PrismaModule,
+    TeamMemberModule,
+    GroupeModule,
+  ],
 })
 export class AppModule {}
