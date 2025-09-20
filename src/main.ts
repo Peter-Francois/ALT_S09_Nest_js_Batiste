2fa016b8bdf460f195270a0184464493088c6782
Peter Francois <peter.francois237@hotmail.com>
2025-09-20 16:07:07 +0200

    feat: create http-exeption.filter for catch http Error and have a custom response

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './utils/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      // whitelist: true,
+      whitelist: true, // when non-whitelisted properties are present example: /formations body {email: test@h.mail}
       // forbidNonWhitelisted: true,
       // transform: true,
       // forbidUnknownValues: true,
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
